03ef4c8a11228159562096f32598d8531cb468f4
Zony Yu <zony249@gmail.com>
2021-01-21 23:02:20 -0700

    completed k-fold cross validation

--- crossVal.py
+++ crossVal.py
@@ -1,8 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+def create_k_folds(tset, k):
+    folds = []
+    for i in range(k):
+        folds.append(tset[i::k, :])
+    
 
-def create_k_folds():
-    pass
+    trainsets = []
+    valsets = []
 
+    concatenate_container = []
+    for i in range(k):
+        for j in range(k):
+            if j == i:
+                valsets.append(folds[j])
+            else:
+                concatenate_container.append(folds[j])
+        trainsets.append(np.concatenate(concatenate_container, axis=0))
+        concatenate_container = []
+    for i in trainsets:
+        print(i.shape)
 
+    for i in valsets:
+        print(i.shape)
+
+    return (trainsets, valsets)
 
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import idx2numpy
-
+from crossVal import *
 
 
 def main():
@@ -15,6 +15,9 @@ def main():
     
     # flattening to 60,000 x 784 matrix 
     trainX = trainX.ravel().reshape((m, rows*cols))
+
+    # [np.array_1, np.array_2 ... np.array_k]
+    k_fold_tset, k_fold_valset = create_k_folds(trainX, 10)
     
     
 
